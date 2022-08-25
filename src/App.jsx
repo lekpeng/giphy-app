300e56d5257efee2aefc4459a474c08590ec0d97
lekpeng <lekpeng96@hotmail.com>
2022-08-25 17:06:06 +0800

    can search for a gif

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import GifDisplay from "./components/GifDisplay";
+import Search from "./components/Search";
 
 function App() {
   const apiKey = "KbEuISuaXSXLHMNJBj853fsn260Ak0Fs";
   const randomGifUrl = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`;
+  const searchGifUrlBase = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=1&q=`;
 
   const [gifUrl, setGifUrl] = useState("");
+  const [searchValue, setSearchValue] = useState("");
 
   const fetchData = async (url) => {
     const response = await fetch(url);
@@ -19,6 +22,12 @@ function App() {
     setGifUrl(data.data.embed_url);
   };
 
+  const fetchSearchGif = async () => {
+    const data = await fetchData(searchGifUrlBase + searchValue);
+    setGifUrl(data.data[0].embed_url);
+  };
+
+  // Mounting
   useEffect(() => {
     (async () => {
       await fetchRandomGif();
@@ -27,11 +36,19 @@ function App() {
     return () => {};
   }, []);
 
+  // Updating
+  useEffect(() => {
+    (async () => {
+      await fetchSearchGif();
+    })();
+  }, [searchValue]);
+
   return (
     <div className="App">
-      <h1>Giphy</h1>
-      <button onClick={fetchRandomGif} className="btn btn-primary mt-5 mb-5" type="button">
-        Get random gif!
+      <h1 className="mb-5">Giphy</h1>
+      <Search setSearchValue={setSearchValue} />
+      <button onClick={fetchRandomGif} className="btn btn-primary mt-3 mb-5" type="button">
+        Or click here to get a random gif!
       </button>
       <GifDisplay gifUrl={gifUrl} />
     </div>
---
+++ src/components/Search.jsx
@@ -0,0 +1,32 @@
+import { useState, useRef } from "react";
+
+const Search = ({ setSearchValue }) => {
+  const searchInput = useRef();
+
+  const handleOnSubmit = (ev) => {
+    ev.preventDefault();
+    const searchValue = searchInput.current.value;
+    setSearchValue(searchValue);
+  };
+
+  return (
+    <form onSubmit={handleOnSubmit}>
+      <div className="row justify-content-center">
+        <div className="col-auto">
+          <input
+            className="form-control"
+            type="text"
+            name="q"
+            ref={searchInput}
+            placeholder="Search for a gif here..."
+          />
+        </div>
+        <div className="col-auto">
+          <input className="btn btn-success" type="submit" />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Search;
